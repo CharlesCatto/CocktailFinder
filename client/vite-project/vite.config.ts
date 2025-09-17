bd71fb2e89c0007943ba3ddff63c9577a619d353
Charles Catto <charlescatto@outlook.fr>
2025-09-17 13:35:46 +0200

    Css adjustment for laptop

--- client/vite-project/vite.config.ts
+++ client/vite-project/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "node:path"; // Correction avec le préfixe node:
+import { resolve } from "node:path";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
